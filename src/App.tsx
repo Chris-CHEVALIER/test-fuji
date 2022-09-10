8799cd769c1cca3557ef29ef4b5d786f3f192f55
Chevalier <chevalierchris34@gmail.com>
2022-09-10 11:18:18 +0200

    connect wallet

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './App.css'
 import FujiForm from './components/FujiForm'
+import { ConnectButton } from '@rainbow-me/rainbowkit'
 
 function App () {
   return (
     <div className='App'>
+      <div className='container'>
+        <ConnectButton />
+      </div>
       <header className='App-header'>
         <FujiForm />
       </header>
--- src/components/FujiForm.tsx
+++ src/components/FujiForm.tsx
@@ -1,41 +1,47 @@
-import React from 'react'
-import './FujiForm.css'
-import { useForm } from 'react-hook-form'
+import React from 'react';
+import { useForm, Resolver } from 'react-hook-form'
+import { verifyMessage } from 'ethers/lib/utils'
+
+type FormValues = {
+  message: string;
+};
+
+const resolver: Resolver<FormValues> = async (values) => {
+  return {
+    values: values.message ? values : {},
+    errors: !values.message
+      ? {
+          message: {
+            type: 'required',
+            message: 'Meow message is required.',
+          },
+        }
+      : {},
+  };
+};
 
 export default function FujiForm () {
-  const { register, handleSubmit } = useForm()
+  const { register, handleSubmit, formState: { errors } } = useForm<FormValues>({ resolver });
 
-  const onSubmit = (data: object) => {
-    console.log(data)
+  const onSubmit = (data: FormValues) => {
+    console.log(data.message);
+    //const address = verifyMessage(data.message, );
   }
 
   return (
     <form className='col s12' onSubmit={handleSubmit(onSubmit)}>
       <div className='row'>
         <div className='input-field'>
-          <input
-            id='username'
-            type='text'
-            className='validate'
-            {...register('username')}
+          <textarea
+            id='message'
+            className='materialize-textarea white-text'
+            {...register('message')}
           />
-          <label htmlFor='usern ame'>Username</label>
+          <label htmlFor='message'>Meow message</label>
         </div>
       </div>
-
-      <div className='row'>
-        <div className='input-field'>
-          <input
-            id='email'
-            type='email'
-            className='validate'
-            {...register('email')}
-          />
-          <label htmlFor='email'>Email</label>
-        </div>
-      </div>
-
-      <button className="waves-effect waves-light btn">Submit</button>
+      <button className='waves-effect waves-light btn'>Send meow</button>
+      {errors?.message && <p>{errors.message.message}</p>}
     </form>
   )
 }
